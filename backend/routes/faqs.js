73c7aeb0e9de1826ed49d13eabf69adc8a36867e
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2025-02-02 15:34:51 +0530

    fix : Cloud Run Optimize

--- backend/routes/faqs.js
+++ backend/routes/faqs.js
@@ -13,7 +13,6 @@ redisClient.connect();
 
 const translate = new Translate({
   projectId: process.env.GOOGLE_PROJECT_ID,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 
 router.get("/", async (req, res) => {
